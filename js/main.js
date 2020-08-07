09f2dfce105bd7310aed0988a52089ee8ffaa5ad
pigpilly <69316089+pigpilly@users.noreply.github.com>
2020-08-08 02:43:40 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener("click", () => {
+  document.getElementById("body").classList.toggle("scroll-lock");
   navLinks.classList.toggle("open");
   links.forEach((link) => {
     link.classList.toggle("fade");
